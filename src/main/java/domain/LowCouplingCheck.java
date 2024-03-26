89e0f9d5c3cdc8e12cb30b310228d5bdf17191e7
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-03-26 13:09:16 -0400

    rechecking old changes

--- src/main/java/domain/LowCouplingCheck.java
+++ src/main/java/domain/LowCouplingCheck.java
@@ -37,35 +37,46 @@ public class LowCouplingCheck extends GraphCheck {
             return new HashSet<Message>();
         }
         parseConfig(config);
-        handleNegativeGraphDegrees();
         checkGraphDegrees();
+        return checkCycles();
+    }
 
+    private Set<Message> checkCycles() {
         if (!checkCycles) {
             return messages;
         }
+        recurseThroughClasses();
+        return messages;
+    }
 
-        // cycle checking
+    private void recurseThroughClasses() {
         ClassGraphIterator it = graph.graphIterator(lowestInDegrees.poll().index);
         recursion(it, messages, lowestInDegrees, ignoreSelf);
         while (!lowestInDegrees.isEmpty()) {
             recursion(graph.graphIterator(lowestInDegrees.poll().index), messages, lowestInDegrees, ignoreSelf);
         }
-
-
-        return messages;
     }
 
     private void checkGraphDegrees() {
+        handleNegativeGraphDegrees();
         for (int i = 0; i < graph.getNumClasses(); i++) {
             boolean alreadyInPackage = packageName != null && graph.getClasses().get(graph.indexToClass(i)).getPackageName().equals(packageName);
             if (alreadyInPackage) continue;
             lowestInDegrees.add(new IntegerAndDegree(i, graph.inDegree(i)));
-            if (graph.inDegree(i) > maxInDegree) {
-                messages.add(new Message(MessageLevel.WARNING, MessageFormat.format("In Degree exceeds {0}, is {1}", maxInDegree, graph.inDegree(i)), graph.indexToClass(i)));
-            }
-            if (graph.outDegree(i) > maxOutDegree) {
-                messages.add(new Message(MessageLevel.WARNING, MessageFormat.format("Out Degree exceeds {0}, is {1}", maxOutDegree, graph.outDegree(i)), graph.indexToClass(i)));
-            }
+            handleExceededMaxInDegree(i);
+            handleExceededMaxOutDegree(i);
+        }
+    }
+
+    private void handleExceededMaxOutDegree(int i) {
+        if (graph.outDegree(i) > maxOutDegree) {
+            messages.add(new Message(MessageLevel.WARNING, MessageFormat.format("Out Degree exceeds {0}, is {1}", maxOutDegree, graph.outDegree(i)), graph.indexToClass(i)));
+        }
+    }
+
+    private void handleExceededMaxInDegree(int i) {
+        if (graph.inDegree(i) > maxInDegree) {
+            messages.add(new Message(MessageLevel.WARNING, MessageFormat.format("In Degree exceeds {0}, is {1}", maxInDegree, graph.inDegree(i)), graph.indexToClass(i)));
         }
     }
 
