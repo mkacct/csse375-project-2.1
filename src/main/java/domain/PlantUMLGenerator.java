ebdf9501d0ecc53a457970e430385bbca3ece6a2
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-03-26 13:39:10 -0400

    re-added credit

--- src/main/java/domain/PlantUMLGenerator.java
+++ src/main/java/domain/PlantUMLGenerator.java
@@ -24,6 +24,7 @@ import net.sourceforge.plantuml.SourceStringReader;
 public class PlantUMLGenerator extends GraphCheck {
     private static final String NAME = "plantUMLGenerator";
 
+    // got this code from the plantuml.com/api
     private static String generateSVG(SourceStringReader source) throws IOException {
         final ByteArrayOutputStream os = new ByteArrayOutputStream();
         @SuppressWarnings({ "deprecation", "unused" })
