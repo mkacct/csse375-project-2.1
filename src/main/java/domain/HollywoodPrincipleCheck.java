c7ca64360b356f46dce6a38cb88e0a82a34eda66
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-04-01 16:34:20 -0400

    svaing progress, this won't pass tests

--- src/main/java/domain/HollywoodPrincipleCheck.java
+++ src/main/java/domain/HollywoodPrincipleCheck.java
@@ -2,7 +2,10 @@ package domain;
 
 import datasource.Configuration;
 import domain.javadata.ClassData;
+import domain.javadata.ClassType;
+import domain.javadata.MethodData;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,6 +16,30 @@ public class HollywoodPrincipleCheck extends Check {
 
   @Override
   public Set<Message> run(Map<String, ClassData> classes, Configuration config) {
-    return null;
+    boolean checkHollywood = config.getBoolean("hollywood", true);
+    if (!config.getBoolean("hollywood", true)) {
+      return Set.of();
+    }
+    return checkAbstractClasses(classes);
   }
+
+  private Set<Message> checkAbstractClasses(Map<String, ClassData> classes) {
+    Set<Message> messages = new HashSet<>();
+    for (Map.Entry<String,ClassData> entry : classes.entrySet()) {
+      if (isNotAbstractClass(entry)) continue;
+      messages.add(checkForAbstractMethods(entry.getValue().getMethods()));
+    }
+    return messages;
+  }
+
+  private static boolean isNotAbstractClass(Map.Entry<String, ClassData> entry) {
+    return !(entry.getValue().isAbstract() && entry.getValue().getClassType() == ClassType.CLASS);
+  }
+
+  private Message checkForAbstractMethods(Set<MethodData> methods) {
+    for (MethodData m : methods) {
+      if (!m.isAbstract()) continue;
+    }
+  }
+
 }
