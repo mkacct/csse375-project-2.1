342f865f7ef1acf062775550e57d82c41039c6b0
Madeline Kahn <kahnmv@rose-hulman.edu>
2024-04-14 21:53:42 -0400

    Make behavior of filtering compiler generated names usable in multiple places

--- src/main/java/domain/javadata/ClassDataCollection.java
+++ src/main/java/domain/javadata/ClassDataCollection.java
@@ -2,12 +2,14 @@ package domain.javadata;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 /**
  * Collection of ClassData indexed by full name, for passing into checks.
+ * Iterating over the collection skips compiler-generated classes, though you can still get them by name.
  */
 public class ClassDataCollection implements Collection<ClassData> {
 	private final Map<String, ClassData> classesByFullName;
@@ -33,7 +35,13 @@ public class ClassDataCollection implements Collection<ClassData> {
 
 	@Override
 	public int size() {
-		return this.classesByFullName.size();
+		int count = 0;
+		for (ClassData classData : this.classesByFullName.values()) {
+			if (!NameUtil.isCompilerGenerated(classData.getFullName())) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 	@Override
@@ -46,19 +54,29 @@ public class ClassDataCollection implements Collection<ClassData> {
 		return this.classesByFullName.containsValue(o);
 	}
 
+	private Set<ClassData> getNonCompilerGeneratedClasses() {
+		Set<ClassData> nonCompilerGeneratedClasses = new HashSet<ClassData>();
+		for (ClassData classData : this.classesByFullName.values()) {
+			if (!NameUtil.isCompilerGenerated(classData.getFullName())) {
+				nonCompilerGeneratedClasses.add(classData);
+			}
+		}
+		return nonCompilerGeneratedClasses;
+	}
+
 	@Override
 	public Iterator<ClassData> iterator() {
-		return this.classesByFullName.values().iterator();
+		return this.getNonCompilerGeneratedClasses().iterator();
 	}
 
 	@Override
 	public Object[] toArray() {
-		return this.classesByFullName.values().toArray();
+		return this.getNonCompilerGeneratedClasses().toArray();
 	}
 
 	@Override
 	public <T> T[] toArray(T[] a) {
-		return this.classesByFullName.values().toArray(a);
+		return this.getNonCompilerGeneratedClasses().toArray(a);
 	}
 
 	@Override
@@ -108,6 +126,10 @@ public class ClassDataCollection implements Collection<ClassData> {
 	}
 
 	public Set<String> getFullNames() {
-		return this.classesByFullName.keySet();
+		Set<String> fullNames = new HashSet<String>();
+		for (ClassData classData : this.getNonCompilerGeneratedClasses()) {
+			fullNames.add(classData.getFullName());
+		}
+		return fullNames;
 	}
 }
