92bd141aee54a799c41583c680f8bf9a8c25e855
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-04-16 20:34:59 -0400

    reset DirLoader back to beginning of project, re-wrote tests to be characterization tests

--- src/main/java/datasource/DirLoader.java
+++ src/main/java/datasource/DirLoader.java
@@ -3,11 +3,7 @@ package datasource;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.security.InvalidParameterException;
-import java.text.MessageFormat;
 import java.util.HashSet;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -21,88 +17,24 @@ public class DirLoader implements FilesLoader {
 	}
 
 	@Override
-	public Set<byte[]> loadFiles(String ext) throws IOException, IllegalStateException {
+	public Set<byte[]> loadFiles(String ext) throws IOException {
 		Set<byte[]> files = new HashSet<byte[]>();
-		File dir = validatePath(ext);
-		addFilesFromDir(files, dir, ext);
+		addFilesFromDir(files, new File(this.path), ext);
 		return files;
 	}
 
-	private void checkForEmptyPath() {
-		if (this.path.isEmpty()) {
-			throw new InvalidParameterException("Empty path is not allowed");
-		}
-	}
-
-	private File validatePath(String ext) {
-		this.checkForEmptyPath();
-		File dir = new File(this.path);
-		handlePathAsNonDirectory(dir);
-		handleEmptyDirectory(dir);
-		handleDirectoryWithoutClassFiles(dir, ext);
-		return dir;
-	}
-
-	private void handleEmptyDirectory(File dir) {
-		if (dir.listFiles() == null || Objects.requireNonNull(dir.listFiles()).length == 0) {
-			throw new IllegalStateException(MessageFormat.format("Provided path is empty: {0}", this.path));
-		}
-	}
-
-	private void handleDirectoryWithoutClassFiles(File dir, String ext) {
-		for (File file : Objects.requireNonNull(dir.listFiles())) {
-			handleNonClassFile(file, ext);
-		}
-	}
-
-	private void handleNonClassFile(File file, String ext) {
-		boolean isNotClassFile = !getExtensionByStringHandling(file.getName()).equals(ext);
-		if (isNotClassFile) {
-			throw new IllegalStateException(MessageFormat.format("{0} is not a class file", path + "/" + file.getName()));
-		}
-	}
-
-	// Source: https://www.baeldung.com/java-file-extension
-	private String getExtensionByStringHandling(String filename) {
-		Optional<String> extension = Optional.ofNullable(filename)
-						.filter(f -> f.contains("."))
-						.map(f -> f.substring(filename.lastIndexOf(".") + 1));
-		return convertOptionalToString(extension);
-	}
-
-	// Source: https://mkyong.com/java8/java-8-convert-optionalstring-to-string/
-	private String convertOptionalToString(Optional<String> extension) {
-		return extension.stream()
-						.filter(x -> x.length() == 1)
-						.findFirst()  // returns Optional
-						.map(Object::toString)
-						.orElse("");
-	}
-
-
-	private void handlePathAsNonDirectory(File dir) {
-		if (!dir.isDirectory()) {
-			throw new IllegalStateException(MessageFormat.format("Provided path is not a directory: {0}", this.path));
-		}
-	}
-
 	private void addFilesFromDir(Set<byte[]> files, File dir, String ext) throws IOException {
-		for (File file : Objects.requireNonNull(dir.listFiles())) {
-			parseFileObject(files, ext, file);
+		if (dir.listFiles() == null) {
+			return;
 		}
-	}
-
-	private void parseFileObject(Set<byte[]> files, String ext, File file) throws IOException {
-		if (file.isDirectory()) {
-			addFilesFromDir(files, file, ext);
-		} else {
-			addClassFile(files, ext, file);
-		}
-	}
-
-	private void addClassFile(Set<byte[]> files, String ext, File file) throws IOException {
-		if (ext == null || file.getName().endsWith("." + ext)) {
-			files.add(readFile(file));
+		for (File file : dir.listFiles()) {
+			if (file.isDirectory()) {
+				addFilesFromDir(files, file, ext);
+			} else {
+				if (ext == null || file.getName().endsWith("." + ext)) {
+					files.add(readFile(file));
+				}
+			}
 		}
 	}
 
--- src/test/java/datasource/DirLoaderTest.java
+++ src/test/java/datasource/DirLoaderTest.java
@@ -2,7 +2,10 @@ package datasource;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.security.InvalidParameterException;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -11,51 +14,36 @@ public class DirLoaderTest {
   private static final String CLASS_FILE_EXT = "class";
 
   @Test
-  public void loadFiles_EmptyPath_ExpectedException() {
+  public void loadFiles_EmptyPath_ExpectedException() throws IOException {
     DirLoader dirLoader = new DirLoader("");
-    Exception exception = assertThrows(InvalidParameterException.class, () -> {
-      dirLoader.loadFiles(CLASS_FILE_EXT);
-    });
-    assertEquals("Empty path is not allowed", exception.getMessage());
+    assertEquals(new HashSet<byte[]>(), dirLoader.loadFiles(CLASS_FILE_EXT));
   }
 
   @Test
-  public void loadFiles_NonDirectoryPath_ExpectedException() {
+  public void loadFiles_NonDirectoryPath_ExpectedException() throws IOException {
     String path = "src/test/resources/DirLoaderNonDirectoryFile.txt";
     DirLoader dirLoader = new DirLoader(path);
-    Exception exception = assertThrows(IllegalStateException.class, () -> {
-      dirLoader.loadFiles(CLASS_FILE_EXT);
-    });
-    assertEquals("Provided path is not a directory: " + path, exception.getMessage());
+    assertEquals(new HashSet<byte[]>(), dirLoader.loadFiles(CLASS_FILE_EXT));
   }
 
   @Test
-  public void loadFiles_NonExistentDirectory_ExpectedException() {
+  public void loadFiles_NonExistentDirectory_ExpectedException() throws IOException {
     String path = "src/test/resources/not_existent_directory";
     DirLoader dirLoader = new DirLoader(path);
-    Exception exception = assertThrows(IllegalStateException.class, () -> {
-      dirLoader.loadFiles(CLASS_FILE_EXT);
-    });
-    assertEquals("Provided path is not a directory: " + path, exception.getMessage());
+    assertEquals(new HashSet<byte[]>(), dirLoader.loadFiles(CLASS_FILE_EXT));
   }
 
   @Test
-  public void loadFiles_NonClassFileDirectory_ExpectedException() {
+  public void loadFiles_NonClassFileDirectory_ExpectedException() throws IOException {
     String path = "src/test/resources/DirLoaderNonClassFileDirectory";
     DirLoader dirLoader = new DirLoader(path);
-    Exception exception = assertThrows(IllegalStateException.class, () -> {
-      dirLoader.loadFiles(CLASS_FILE_EXT);
-    });
-    assertEquals("src/test/resources/DirLoaderNonClassFileDirectory/hello.txt is not a class file", exception.getMessage());
+    assertEquals(new HashSet<byte[]>(), dirLoader.loadFiles(CLASS_FILE_EXT));
   }
 
   @Test
-  public void loadFiles_EmptyDirectory_ExpectedException() {
+  public void loadFiles_EmptyDirectory_ExpectedException() throws IOException {
     String path = "src/test/resources/DirLoaderEmptyDirectory";
     DirLoader dirLoader = new DirLoader(path);
-    Exception exception = assertThrows(IllegalStateException.class, () -> {
-      dirLoader.loadFiles(CLASS_FILE_EXT);
-    });
-    assertEquals("Provided path is empty: " + path, exception.getMessage());
+    assertEquals(new HashSet<byte[]>(), dirLoader.loadFiles(CLASS_FILE_EXT));
   }
 }
