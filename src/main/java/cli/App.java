bf7f1a40999e87b2d260b6534951afb7ef0634c9
Madeline Kahn <kahnmv@rose-hulman.edu>
2024-04-26 00:36:59 -0400

    Extract behavior from cli.App that is not explicitly CLI-related into new domain.CheckUtil

--- src/main/java/cli/App.java
+++ src/main/java/cli/App.java
@@ -13,17 +13,12 @@ import datasource.ConfigLoader;
 import datasource.Configuration;
 import datasource.FilesLoader;
 import domain.Check;
+import domain.CheckUtil;
 import domain.Message;
 import domain.MessageLevel;
-import domain.javadata.ClassData;
 import domain.javadata.ClassDataCollection;
-import domain.javadata.ClassReaderUtil;
 
 class App {
-	private static final String CLASS_FILE_EXT = "class";
-	private static final String ENABLE_KEY_PREFIX = "enable_";
-	private static final String SKIP_UNMARKED_CHECKS_KEY = "skipUnmarkedChecks";
-
 	private static final Map<MessageLevel, TerminalTextColor> MESSAGE_LEVEL_COLORS = Map.of(
 		MessageLevel.ERROR, TerminalTextColor.RED,
 		MessageLevel.WARNING, TerminalTextColor.YELLOW,
@@ -47,7 +42,7 @@ class App {
 		Set<byte[]> classFiles;
 		Configuration config;
 		try {
-			classFiles = this.filesLoader.loadFiles(CLASS_FILE_EXT);
+			classFiles = this.filesLoader.loadFiles(CheckUtil.CLASS_FILE_EXT);
 		} catch (IllegalStateException ex) { // dir not found
 			this.errStream.println(ex.getMessage());
 			return false;
@@ -66,65 +61,23 @@ class App {
 			}
 		}
 
-		ClassDataCollection classes = readInClasses(classFiles);
-		Map<MessageLevel, Integer> msgTotals = this.runAllChecksAndPrintResults(checks, classes, config);
+		ClassDataCollection classes = CheckUtil.readInClasses(classFiles);
+		Map<MessageLevel, Integer> msgTotals = new HashMap<MessageLevel, Integer>();
+		int numChecksRun = CheckUtil.runAllChecks(checks, classes, config, msgTotals, this::printCheckResults);
+		this.outStream.println(MessageFormat.format("Checks run: {0}", numChecksRun));
+		this.printTotals(msgTotals);
 		return msgTotals.get(MessageLevel.ERROR) == 0;
 	}
 
-	private static ClassDataCollection readInClasses(Set<byte[]> classFiles) {
-		ClassDataCollection classes = new ClassDataCollection();
-		for (byte[] classFile : classFiles) {
-			ClassData classData = ClassReaderUtil.read(classFile);
-			classes.add(classData);
+	private void printCheckResults(String checkName, Set<Message> generatedMsgs) {
+		this.outStream.println(MessageFormat.format("Check {0}:", checkName));
+		for (Message msg : generatedMsgs) {
+			this.outStream.println(MessageFormat.format("\t{0}", colorMessageTag(msg)));
 		}
-		return classes;
-	}
-
-	private Map<MessageLevel, Integer> runAllChecksAndPrintResults(
-		Check[] checks, ClassDataCollection classes, Configuration config
-	) {
-		boolean skipUnmarked = Boolean.TRUE.equals(this.configBoolOrNull(config, SKIP_UNMARKED_CHECKS_KEY));
-		Map<MessageLevel, Integer> msgTotals = initMsgTotals();
-		int numChecksRun = 0;
-		for (Check check : checks) {
-			Boolean configVal = this.configBoolOrNull(config, ENABLE_KEY_PREFIX + check.name);
-			if (skipUnmarked ? Boolean.TRUE.equals(configVal) : check.isEnabled(configVal)) {
-				this.runCheckAndPrintResults(check, classes, config, msgTotals);
-				numChecksRun++;
-			}
+		if (generatedMsgs.isEmpty()) {
+			this.outStream.println(MessageFormat.format("\t{0}",TerminalTextColor.BLACK.applyTo("(no messages)")));
 		}
-		this.outStream.println(MessageFormat.format("Checks run: {0}", numChecksRun));
-		this.printTotals(msgTotals);
-		return msgTotals;
-	}
-
-	private Boolean configBoolOrNull(Configuration config, String key) {
-		try {
-			return config.getBoolean(key);
-		} catch (IllegalArgumentException ex) {
-			return null;
-		} catch (ClassCastException ex) {
-			this.errStream.println(MessageFormat.format(
-				"Error in config: property \"{0}\", if present, should be boolean",
-				key
-			));
-			return null;
-		}
-	}
-
-	private void runCheckAndPrintResults(
-		Check check, ClassDataCollection classes, Configuration config, Map<MessageLevel, Integer> msgTotals
-	) {
-		Set<Message> generatedMsgs = check.run(classes, config);
-			this.outStream.println(MessageFormat.format("Check {0}:", check.name));
-			for (Message msg : generatedMsgs) {
-				msgTotals.put(msg.level, msgTotals.get(msg.level) + 1);
-				this.outStream.println(MessageFormat.format("\t{0}", colorMessageTag(msg)));
-			}
-			if (generatedMsgs.isEmpty()) {
-				this.outStream.println(MessageFormat.format("\t{0}",TerminalTextColor.BLACK.applyTo("(no messages)")));
-			}
-			this.outStream.println();
+		this.outStream.println();
 	}
 
 	private static String colorMessageTag(Message msg) {
@@ -157,13 +110,5 @@ class App {
 				MessageFormat.format("{0} {1}", count, level.abbreviation)
 			));
 		}
-	}
-
-	private static Map<MessageLevel, Integer> initMsgTotals() {
-		Map<MessageLevel, Integer> msgTotals = new HashMap<>();
-		for (MessageLevel level : MessageLevel.values()) {
-			msgTotals.put(level, 0);
-		}
-		return msgTotals;
 	}
 }
